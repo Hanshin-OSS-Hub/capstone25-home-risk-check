c81a6e19348670ddbb951d1fb01c397b723f613c
kwangho <kevin7517@naver.com>
2026-02-08 13:38:26 +0900

    chore: 프로젝트 구조 업데이트

---
+++ src/backend/home-risk-check-backend/src/main/java/hanshin/homeriskcheck/home_risk_check_backend/HomeRiskCheckBackendApplication.java
@@ -0,0 +1,13 @@
+package hanshin.homeriskcheck.home_risk_check_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeRiskCheckBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeRiskCheckBackendApplication.class, args);
+	}
+
+}

214b28b2643e6d198fcec1c4e3474e2dd8c7f297
imnerf242 <sangyoon0005301@gmail.com>
2026-03-05 23:20:37 +0900

    feat : 필요없는 엔티티 제거

--- backend/home-risk-check-spring/src/main/java/hanshin/home_risk_check/safetyscore/domain/crime/repository/CrimeRepository.java
+++
@@ -1,8 +0,0 @@
-package hanshin.home_risk_check.safetyscore.domain.crime.repository;
-
-
-import hanshin.home_risk_check.safetyscore.domain.crime.entity.CrimeStat;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CrimeRepository extends JpaRepository<CrimeStat, Long> {
-}

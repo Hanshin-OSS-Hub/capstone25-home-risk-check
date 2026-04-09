ebbae90880bbd875df7c2eb9253e2dd540e1bbe3
kwangho <kevin7517@naver.com>
2026-04-09 12:41:29 +0900

    feat: 유저 정보를 다루는 기본 객체 구현

---
+++ backend/home-risk-check-spring/src/main/java/hanshin/home_risk_check/user/entity/CustomUserDetails.java
@@ -0,0 +1,46 @@
+package hanshin.home_risk_check.user.entity;
+
+import lombok.NonNull;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class CustomUserDetails implements UserDetails {
+
+    private final User user;
+
+    public CustomUserDetails(User user) {
+        this.user = user;
+    }
+
+    @Override
+    @NonNull
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> collection = new ArrayList<>();
+
+        collection.add(new GrantedAuthority() {
+            @Override
+            public String getAuthority() {
+                return user.getRole().name();
+            }
+        });
+        return collection;
+    }
+
+    @Override
+    public String getPassword() { return user.getPasswordHash(); }
+
+    @Override
+    @NonNull
+    public String getUsername() { return user.getEmail(); }
+
+    @Override public boolean isAccountNonExpired()  { return true; }
+    @Override public boolean isAccountNonLocked()   { return true; }
+    @Override public boolean isCredentialsNonExpired() { return true; }
+    @Override public boolean isEnabled()            { return true; }
+
+    public Long getUserId() { return user.getId(); }
+    public String getNickname() { return user.getNickname(); }
+}
